daa19a97e3d9e76af1c5d94262ae89d901c9a8f0
Christina Hartono <94892811+christinahrt@users.noreply.github.com>
2021-12-07 09:52:20 +0800

    Rev fungsi

--- Timezone.cpp
+++ Timezone.cpp
@@ -2,127 +2,167 @@
 #include <stdlib.h>
 #include <string.h>
 void welcome();
-void verifikasi(int p, int *inputPin);
-void saldoTZ(int *saldoAkun, int saldoSisa);
-void noKartu(int *noRek, int kartuAkun);
+void header();
+void verifikasi(int pin);
 void tampilkanStrukTimezone();
 void menu();
-char *akunTZ, akun1[]="Christina Hartono", akun2[]="Benita Carissa";
-int input, totalsaldo;
-int pin, pin1 = 1122, pin2 = 2233;
-int saldo = 0, saldo1 = 500000, saldo2 = 750000;
-int kartu = 0, kartu1 = 221017, kartu2 = 568635;
 
+int loginedIndex;
+int memberSize = 2;
 
-void verifikasi(int p, int *inputPin)
-{
-        char **akunAktif;
-        if(p == pin1)
-        {
-                *inputPin = p;
-                akunAktif = &akunTZ;
-                *akunAktif = (char*) malloc (100 * sizeof(char));
-                strcpy(*akunAktif, akun1);
-                printf("\nNama Pemilik : %s\n", akunTZ);
-                saldoTZ(&saldo,saldo1);
-                printf("Jumlah Saldo : Rp %d\n", saldo);
-                noKartu(&kartu,kartu1);
-                printf("Nomer Kartu :%d\n", kartu);
-              	menu();
-         }
-         
-        else if(p == pin2)
-        {
-                *inputPin = p;
-                akunAktif = &akunTZ;
-                *akunAktif = (char*) malloc (100 * sizeof(char));
-                strcpy(*akunAktif, akun2);
-                printf("\nNama Pemilik : %s\n", akunTZ);
-                saldoTZ(&saldo,saldo2);
-                printf("Jumlah Saldo : Rp %d\n", saldo);
-                noKartu(&kartu,kartu2);
-                printf("Nomer Kartu :%d\n", kartu);
-              	menu();
-         }
-         else
-         {
-                welcome();
-         }
+struct Member{
+    char nama[50];
+    int pin;
+    int kartu;
+    int saldo;
+};
+
+struct Member members[] = {
+	{"Maria Christina Hartono", 1122, 221017, 500000},
+	{"Benita", 2233, 568635, 750000},
+};
+
+int main(){
+    header();
+	welcome(); //Fungsi untuk tampilan utama
+	menu();
+//	printf("Namaku adalah %s\n", members[loginedIndex].nama);
+//	printf("Saldo awal adalah %d\n", members[loginedIndex].saldo);
+//	members[0].saldo += 2;
+//	printf("Saldo akhir adalah %d\n\n", members[0].saldo);
+//	printf("Masukkan nama: ");
+//	scanf(" %s", &members[memberSize].nama);
+//	printf("Masukkan kode: ");
+//	scanf(" %s", &members[memberSize].pin);
+//	printf("Masukkan no kartu: ");
+//	scanf(" %s", &members[memberSize].kartu);
+//	printf("Saldo anda: 0\n");
+
+//	strcpy(members[memberSize].nama, "Jodie Mantra");
+//	members[memberSize].nama = "Jodie Mantra";
+//	members[memberSize].pin = 123;
+//	members[memberSize].kartu = 123;
+//	members[memberSize].saldo = 0;
+//	
+//	printf("Nama doiku adalah %s\n", members[2].nama);
+//	
+//	memberSize++;
+	return 0;
 }
 
-int main()
-{
-        welcome(); //Fungsi untuk tampilan utama
-return 0;
-}
-void welcome()
-{
-        int inputPin; //Deklarasi variabel untuk input pin
-		printf("===============================================================================================\n");
-		printf("\t\t\t\t  SELAMAT DATANG DI TIMEZONE \n");
-		printf("===============================================================================================\n\n");
-        printf("Masukkan password anda :");
-		scanf("%d", &inputPin);
- //Memasukkan pin
-        verifikasi(inputPin, &pin);
- //Memanggil fungsi verifikasi dari file header 'fungsi.h'
+void header (){
+    system ("cls");
+	printf  ("=====================================================\n");	
+	printf  ("                TUGAS BESAR PEMROGRAMAN              \n");
+	printf  ("             PROGRAM TOP UP SALDO TIMEZONE           \n");
+	printf  ("=====================================================\n");
+	printf  ("          Program disusun dan dibuat oleh :          \n");
+	printf  ("                                                     \n");
+	printf  ("  1. Benita Carissa Sutrisno Putri (2105551002)      \n");
+	printf  ("  2. Maria Christina Hartono       (2105551010)      \n");
+	printf  ("                                                     \n");
+	printf  ("             Jurusan Teknologi Informasi             \n");
+	printf  ("                   Fakultas Teknik                   \n");
+	printf  ("                 Universitas Udayana                 \n");
+	printf  ("                       2021                          \n");
+	printf  ("=====================================================\n");
+	printf  ("          Tekan ENTER  untuk melanjutkan....         \n");
+    printf  ("=====================================================\n");
+	getchar ();
+    system ("cls");
 }
 
-void saldoTZ(int *saldoAkun, int saldoSisa)
-{
-  *saldoAkun = saldoSisa;
+void welcome(){
+	mengulang:
+	system("cls");
+	
+    int inputPin; //Deklarasi variabel untuk input pin
+    
+    printf  ("=====================================================\n");	
+	printf  ("              SELAMAT DATANG DI TIMEZONE             \n");
+	printf  ("=====================================================\n");
+    
+    
+//	printf("===============================================================================================\n");
+//	printf("\t\t\t\t  SELAMAT DATANG DI TIMEZONE \n");
+//	printf("===============================================================================================\n\n");
+//	
+	//Memasukkan pin
+	printf  ("Masukkan password anda   : ");
+	scanf("%d", &inputPin);
+	fflush(stdin);
+	
+    verifikasi(inputPin);
 }
 
-void noKartu(int *noRek, int kartuAkun) 
-{
-  *noRek = kartuAkun;
+void verifikasi(int pin){
+	int i;
+	
+	for (i = 0; i <memberSize ; ++i){
+		if (pin == members[i].pin){
+			loginedIndex = i;
+		}
+	}
+	
+	printf  ("Nama Pemilik 			 : %s                          \n", members[loginedIndex].nama);
+	printf  ("Jumlah Saldo			 : Rp %d                       \n", members[loginedIndex].saldo);
+  printf  ("Nomer Kartu 			 : %d                          \n", members[loginedIndex].kartu);
+
+	printf  ("                                                     \n");
+	printf  ("=====================================================\n");
+	printf  ("          Tekan ENTER  untuk melanjutkan....         \n");
+    printf  ("=====================================================\n");
+
+	
+	
+//	printf("\nNama Pemilik : %s\n", members[loginedIndex].nama);
+//	printf("Jumlah Saldo : Rp %d\n", members[loginedIndex].saldo);
+//    printf("Nomer Kartu :%d\n", members[loginedIndex].kartu);
+//    printf("tekan enter untuk melanjutkan\n");
+//	getchar ();
+    system ("cls");
 }
 
 void menu(){
 	int input;
 
-printf("\n\n");
-printf("===============================================================================================\n");
-printf("\t\t\t\t\t  MENU UTAMA \n");
-printf("===============================================================================================\n\n");
-printf("  1. 50.000\n");
-printf("  2. 100.000\n");
-printf("  3. 150.000 bonus 50.000\n");
-printf("  4. 200.000 bonus 150.000\n");
-printf("  5. 300.000 bonus 300.000\n");
-printf("\n\nPILIHAN : ");
-scanf("%d", &input);
-switch(input)
-{
-	case 1 : //nilai variabel input = 1
-  		totalsaldo = saldo + 50000;
-  		printf("\nTotal Saldo: Rp.%d", totalsaldo);
-		break; //Melompat ke baris setelah bagian akhir dari switch
-		
-	case 2 : //nilai variabel input = 2
-		totalsaldo = saldo + 100000;
-		printf("\nTotal Saldo: Rp.%d", totalsaldo);
-		break; //Melompat ke baris setelah bagian akhir dari switch
-		
-	case 3 : //nilai variabel input = 3
-  		totalsaldo = saldo + 150000 + 50000;
-  		printf("\nTotal Saldo: Rp.%d", totalsaldo);
-		break;
-
-	case 4 : //nilai variabel input = 4
-  		totalsaldo = saldo + 200000 + 150000;
-  		printf("\nTotal Saldo: Rp.%d", totalsaldo);
-		break;
-
-	case 5 : //nilai variabel input = 5
-  		totalsaldo = saldo + 300000 + 300000;
-  		printf("\nTotal Saldo: Rp.%d", totalsaldo);
-		break;
-
-    default:
-
-    break;
-    
-}
+	printf  ("\n\n");
+    printf  ("=====================================================\n");	
+	printf  ("     					 MENU UTAMA     	     	   \n");
+	printf  ("=====================================================\n");
+	printf  ("  2. 100.000\n");
+	printf  ("  3. 150.000 bonus 50.000\n");
+	printf  ("  4. 200.000 bonus 150.000\n");
+	printf  ("  5. 300.000 bonus 300.000\n");
+	printf  ("\n\nPILIHAN : ");
+	scanf("%d", &input);
+	
+	switch(input)
+	{
+		case 1 : //nilai variabel input = 1
+	  		members[loginedIndex].saldo += 50000;
+	  		printf("\nTotal Saldo: Rp.%d", members[loginedIndex].saldo);
+			break; //Melompat ke baris setelah bagian akhir dari switch
+			
+		case 2 : //nilai variabel input = 2
+	  		members[loginedIndex].saldo += 100000;
+			printf("\nTotal Saldo: Rp.%d", members[loginedIndex].saldo);
+			break; //Melompat ke baris setelah bagian akhir dari switch
+			
+		case 3 : //nilai variabel input = 3
+	  		members[loginedIndex].saldo += 150000 + 50000;
+	  		printf("\nTotal Saldo: Rp.%d", members[loginedIndex].saldo);
+			break;
+	
+		case 4 : //nilai variabel input = 4
+	  		members[loginedIndex].saldo += 200000 + 150000;
+	  		printf("\nTotal Saldo: Rp.%d", members[loginedIndex].saldo);
+			break;
+	
+		case 5 : //nilai variabel input = 5
+	  		members[loginedIndex].saldo += 300000 + 300000;
+	  		printf("\nTotal Saldo: Rp.%d", members[loginedIndex].saldo);
+			break;
+	}
 }
 
